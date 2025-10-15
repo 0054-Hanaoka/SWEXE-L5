d3ee6eb39c138d4e5747cbf563256d6c6473499d
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-15 08:05:33 +0000

    ユーザ追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(uid: params[:user][:uid],pass: params[:user][:pass])
+    redirect_to root_path
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:uid, :pass, :age)
+  end
+end
